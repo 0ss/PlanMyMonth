49e8659c7774d0d9bdc6337b148c20ff0e5f19ed
salah <s7baijan@hotmail.com>
2021-03-07 20:48:36 +0300

    * linguist-vendored

--- src/main.js
+++ src/main.js
@@ -10,16 +10,5 @@ createApp(App).use(store).use(router).mount('#app')
 //   for(let i = 0; i < cols.length; i++) {
 //     cols[i].style.backgroundColor = 'white';
 // }
-document.documentElement.style.setProperty('text-white', 'black');
-
-const cols = document.getElementsByClassName('text-white');
-cols.forEach(e => {
-    console.log(e)
-});
-for(var i = 0; i < cols.length; i++) {
-     console.log('s',cols[i])
-    cols[i].backgroundColor = 'black';
-}
-
 
 //console.log(store.state.darkMode, document.getElementsByClassName('dark-bg'))
