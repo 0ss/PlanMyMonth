ad9966e86ffa7fba4dbb4e994fb331762741b31c
salah <s7baijan@hotmail.com>
2021-03-08 18:26:46 +0300

    fix % digit

--- src/views/Stat/StatProject.vue
+++ src/views/Stat/StatProject.vue
@@ -72,7 +72,7 @@ export default {
     },
     methods: {
         getPrecent(tasks, finished){
-            return `${ String((finished/tasks)*100).substring(0,3) }%`
+            return `${ String((finished/tasks)*100).substring(0,4) }%`
         }
     }
 }
